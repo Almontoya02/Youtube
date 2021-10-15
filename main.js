ff582be1aa8e807f8f05571523bdb0642ead383f
Almontoya02 <42153887+Almontoya02@users.noreply.github.com>
2021-10-14 22:09:12 -0500

    update navbar 3

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-var myModal = document.getElementById('exampleModal')
-var myInput = document.getElementById('Mic')
+import bootstrap from "bootstrap"
+var myModal = document.getElementById('exampleModal');
+var myInput = document.getElementById('Mic');
+
 
 myModal.addEventListener('shown.bs.modal', function () {
   myInput.focus()
-  console.log("hola")
+
 })
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+
